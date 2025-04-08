e71f58ecaf5ea9bc9b3fe7f8f4b9415b3f780a06
Ben Prothe <60143238+xxblincolnxx@users.noreply.github.com>
2025-04-08 13:07:58 -0400

    added comment

--- src/util/api/TMDB.ts
+++ src/util/api/TMDB.ts
@@ -8,6 +8,7 @@ export async function getMovies(
   page: number = 1,
   signal: AbortSignal
 ): Promise<MovieResponse> {
+  // IDK why I need to declare options like this instead of just passing signal directly but it doesn't work when I do it that way.
   const options = { signal: signal };
   try {
     const response = await axios.get<MovieResponse>(
